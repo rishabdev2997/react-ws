bc332732b6ee65549fa57c5976c1ab7150083d64
rishab <rishabstudent2000@gmail.com>
2024-11-08 21:12:09 +0530

    react ws

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [socket, setSocket] = useState<null | WebSocket>(null);
+  const [latestMessage, setLatestMessage] = useState("");
+
+  useEffect(() => {
+    const socket = new WebSocket('ws://localhost:8080');
+    socket.onopen = () =>{
+      console.log('Connection established')
+    }
+    socket.onmessage = (message) =>{
+      console.log('Message received: ', message.data)
+      setLatestMessage(message.data)
+    }
+    setSocket(socket);
+  },[]) 
+
+  if(!socket){
+    return <div>
+      Loading...
+    </div>
+  }
+
+  return <div>
+    {latestMessage}
+  </div>
+}
+
+export default App
